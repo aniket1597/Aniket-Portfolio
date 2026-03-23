9d9a44525dee7c50ec065ce1c41ae753877e9d71
aniket1597 <aniketagrawal733@gmail.com>
2026-03-23 10:41:55 +0000

    fix(work): prevent image overflow into adjacent carousel slides

--- dist/assets/Navbar-dqr0fzmu.js
+++ dist/assets/Navbar-dqr0fzmu.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-9mFnS9o3.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j,r as bt}from"./index-BofpJ-Sq.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
